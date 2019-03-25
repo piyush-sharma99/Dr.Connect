bd9905c3caf8650023eb7695543cc7c6a4de6976
dylanm29799 <dyevki@gmail.com>
2019-03-25 14:43:39 +0000

    Test if of the kogin

--- Dr.Connect-master/public/Login.js
+++ Dr.Connect-master/public/Login.js
@@ -1,29 +1,62 @@
 
+/*firebase.auth().onAuthStateChanged(function(user) {
+	if(user){
+	//user signed in
+	 window.location.href = "doctor.html";
+	} else {
+	//no user is signed in
+	alert = "Wrong Email/Password";
+	}
+)};
+function login(){
 
-function login()
-{
-
-var userEmail = document.getElementById("demail_field").value;
-var userPass = document.getElementById("dpass_field").value;
+var userEmail = document.getElementById("demail").value;
+var userPass = document.getElementById("dpass").value;
 
 firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-  window.location.href = "doctor.html";
-    } else {
-      // No user is signed in.
-    }
-  });
-
-  window.alert("Error : "+ errorMessage);
-
-
+  
+  window.alert{"Error: "  + errormessage};
+  // ...
 });
 
 
+}*/
 
-}
+(function(){
+	var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: 'doctor.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: 'doctor.html'
+  
+  
+};
+ui.start('#firebaseui-auth-container', uiConfig);
+})()
