674464f12a7743c61975b61bf951ca6cc1180bc0
piyush-sharma99 <33561037+piyush-sharma99@users.noreply.github.com>
2019-04-08 20:20:07 +0100

    apponiment upload

--- Dr.Connect-master/public/appointment.js
+++ Dr.Connect-master/public/appointment.js
@@ -1,11 +1,37 @@
-function appointment(){
+
   // Get a reference to the database service
-  var database = firebase.database();
+  var db = firebase.firestore();
   
+  var currentUser = firebase.auth().currentUser;
+
+
+  firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+     var example = user.email;
+      var docRef = db.doc("Doctor/"+example+"Reservation/");
+      docRef.get().then(function (doc) {
+          if (doc.exists) {
+              const listitem = document.querySelector("#list2-item");
+              listitem.innerHTML = "<div id=list2-item><p>" + doc.data().date + " " + doc.data().name + "<p></div>";
+              // alert(JSON.stringify(doc.data(), null, 4));
+              console.log("Document data:", doc.data());
+      
+      
+          } else {
+              // doc.data() will be undefined in this case
+              console.log("No such document!");
+          }
+      }).catch(function (error) {
+          console.log("Error getting document:", error);
+      });
+          } else {
+            // No user is signed in.
+          }
+        });
+      
 
 
 
 
 
 
-}
