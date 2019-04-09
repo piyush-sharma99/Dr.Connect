46b1925a39cccb63451039b0559ea235100ceb2b
dylanm29799 <dyevki@gmail.com>
2019-04-09 19:45:51 +0100

    push

--- Dr.Connect-master/public/appointment.js
+++ Dr.Connect-master/public/appointment.js
@@ -1,11 +1,75 @@
-function appointment(){
-  // Get a reference to the database service
-  var database = firebase.database();
+  var app_firebase = {};
+  (function () {
+      // Initialize Firebase
+      var config = {
+          apiKey: "AIzaSyD-eemQvHniRe-5D6p2aY0A32PYajdm8yg",
+          authDomain: "doctor-connect-5.firebaseapp.com",
+          databaseURL: "https://doctor-connect-5.firebaseio.com",
+          projectId: "doctor-connect-5",
+          storageBucket: "doctor-connect-5.appspot.com",
+          messagingSenderId: "358747977546"
+      };
+      firebase.initializeApp(config);
+  
+      app_firebase = firebase;
+  })()
+  
+  
+  var db = firebase.firestore();
+  
+  var currentUser = firebase.auth().currentUser;
+  
+  
+  /*firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+     var example = user.email;
+     var useremail = usersemail
+      
+      }).catch(function (error) {
+          console.log("Error getting document:", error);
+      });
+          } else {
+            // No user is signed in.
+          }
+        });*/
+      
+      //  var x = document.querySelectorAll;
+  
+  //const name = document.querySelector("#docname");
+ // var docRef = db.collection("Doctor").document("dylan22@gmail.com").collection("Appointment").document(documentReference.getId());
+    //  docRef.get().then(function (doc) {
+      //    if (doc.exists) {
+             // const listitem = document.querySelector("#list-item");
+             // listitem.innerHTML = "<div id=list-item><p>" + doc.data().Fname + " "+ doc.data().Gay + " " + "</p></div>";
+              // alert(JSON.stringify(doc.data(), null, 4));
+             
+       //       console.log("Document data:", doc.data());
+      
+      
+      //    } else {
+              // doc.data() will be undefined in this case
+      //        console.log("No such document!");
+      //    }
+     //   })
+
+
+        db.collection("Doctor/dylan22@gmail.com/Appointment").get().then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+              // doc.data() is never undefined for query doc snapshots
+              console.log(doc.id, " => ", doc.data());
+              const listitem = document.querySelector("#list-item");
+              listitem.innerHTML += "<div><p>"+ doc.data().Name + " " + doc.data().Gay + "</p></div>";
+
+          });
+      });
+        
+  
+  
+  
+  
+  
   
 
 
 
 
-
-
-}
