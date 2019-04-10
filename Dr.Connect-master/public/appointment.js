cb7bbc0cc0528d3770d296a2d7695ce3a7a60c2a
piyush-sharma99 <33561037+piyush-sharma99@users.noreply.github.com>
2019-04-10 15:30:42 +0100

    fixing github

--- Dr.Connect-master/public/appointment.js
+++ Dr.Connect-master/public/appointment.js
@@ -1,77 +1,49 @@
 var app_firebase = {};
-(function () {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD-eemQvHniRe-5D6p2aY0A32PYajdm8yg",
-    authDomain: "doctor-connect-5.firebaseapp.com",
-    databaseURL: "https://doctor-connect-5.firebaseio.com",
-    projectId: "doctor-connect-5",
-    storageBucket: "doctor-connect-5.appspot.com",
-    messagingSenderId: "358747977546"
-  };
-  firebase.initializeApp(config);
+  (function () {
+      // Initialize Firebase
+      var config = {
+          apiKey: "AIzaSyD-eemQvHniRe-5D6p2aY0A32PYajdm8yg",
+          authDomain: "doctor-connect-5.firebaseapp.com",
+          databaseURL: "https://doctor-connect-5.firebaseio.com",
+          projectId: "doctor-connect-5",
+          storageBucket: "doctor-connect-5.appspot.com",
+          messagingSenderId: "358747977546"
+      };
+      firebase.initializeApp(config);
 
-  app_firebase = firebase;
-})()
+      app_firebase = firebase;
+  })()
 
 
-var db = firebase.firestore();
+  var db = firebase.firestore();
 
-var currentUser = firebase.auth().currentUser;
+  var currentUser = firebase.auth().currentUser;
 
-<<<<<<< HEAD
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
- var example = user.email;
-  var docRef = db.doc("Doctor/"+ example);
-  docRef.get().then(function (doc) {
-      if (doc.exists) {
-          db.collection("Doctor/"+ example +"/Appointment").get().then(function(querySnapshot) {
-              querySnapshot.forEach(function(doc) {
-                  // doc.data() is never undefined for query doc snapshots
-                  console.log(doc.id, " => ", doc.data());
-                  const listitem = document.querySelector("#list-item");
-                  listitem.innerHTML += "<div id=list-item><p>"+ doc.data().name + " = " + doc.data().date + "</p></div>";
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+   var example = user.email;
+    var docRef = db.doc("Doctor/"+ example);
+    docRef.get().then(function (doc) {
+        if (doc.exists) {
+            db.collection("Doctor/"+ example +"/Appointment").get().then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, " => ", doc.data());
+                    const listitem = document.querySelector("#list-item");
+                    listitem.innerHTML += "<div><p>"+ doc.data().name + " = " + doc.data().date + "</p></div>";
+      
+                });
+            });
     
-              });
-          });
-  
-  
-      } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-      }
-  }).catch(function (error) {
-      console.log("Error getting document:", error);
-  });
-      } else {
-        // No user is signed in.
-      }
+    
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
     });
-=======
-
-  /*firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-     var example = user.email;
-     var useremail = usersemail
-
-      }).catch(function (error) {
-          console.log("Error getting document:", error);
+        } else {
+          // No user is signed in.
+        }
       });
-          } else {
-            // No user is signed in.
-          }
-        });
-      */
-
-        db.collection("Doctor/dylan22@gmail.com/Appointment").get().then(function(querySnapshot) {
-          querySnapshot.forEach(function(doc) {
-              // doc.data() is never undefined for query doc snapshots
-              console.log(doc.id, " => ", doc.data());
-              const listitem = document.querySelector("#list-item");
-              listitem.innerHTML += "<div><p>"+ doc.data().Name + " " + doc.data().Gay + "</p></div>";
-
-          });
-      });
->>>>>>> parent of 29accb5... major changes includes all main features
