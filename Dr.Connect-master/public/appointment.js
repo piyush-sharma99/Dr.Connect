9ef83411f266251b6d81f09cb762252e4e6fad9f
dylanm29799 <dyevki@gmail.com>
2019-04-10 12:25:59 +0100

    Revert "major changes includes all main features"
    This reverts commit 29accb57c4df9c3675f7c8b4b2021929ccffd905.

--- Dr.Connect-master/public/appointment.js
+++ Dr.Connect-master/public/appointment.js
@@ -19,34 +19,27 @@
 
   var currentUser = firebase.auth().currentUser;
 
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-   var example = user.email;
-    var docRef = db.doc("Doctor/"+ example);
-    docRef.get().then(function (doc) {
-        if (doc.exists) {
-            db.collection("Doctor/"+ example +"/Appointment").get().then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                    // doc.data() is never undefined for query doc snapshots
-                    console.log(doc.id, " => ", doc.data());
-                    const listitem = document.querySelector("#list-item");
-                    listitem.innerHTML += "<div><p>"+ doc.data().name + " = " + doc.data().date + "</p></div>";
-      
-                });
-            });
-    
-    
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch(function (error) {
-        console.log("Error getting document:", error);
-    });
-        } else {
-          // No user is signed in.
-        }
-      });
-    
 
-      
+  /*firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+     var example = user.email;
+     var useremail = usersemail
+
+      }).catch(function (error) {
+          console.log("Error getting document:", error);
+      });
+          } else {
+            // No user is signed in.
+          }
+        });
+      */
+
+        db.collection("Doctor/dylan22@gmail.com/Appointment").get().then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+              // doc.data() is never undefined for query doc snapshots
+              console.log(doc.id, " => ", doc.data());
+              const listitem = document.querySelector("#list-item");
+              listitem.innerHTML += "<div><p>"+ doc.data().Name + " " + doc.data().Gay + "</p></div>";
+
+          });
+      });
