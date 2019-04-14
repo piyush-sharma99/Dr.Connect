18f3411c9e751b6c81f94d1517c7f7cec6e7cd50
dylanm29799 <dyevki@gmail.com>
2019-04-14 17:52:55 +0100

    dd

--- Dr.Connect-master/public/prescription.js
+++ Dr.Connect-master/public/prescription.js
@@ -6,20 +6,22 @@ function prescription(){
 var db = firebase.firestore();
 
 
-	var inputPatID = document.getElementById("patID_field").value;
+	var inputpatemail = document.getElementById("patEmail_field").value;
 	var inputDocnum = document.getElementById("docnum_field").value;
 	var inputMed1 = document.getElementById("med1_field").value;
 	var inputMed2 = document.getElementById("med2_field").value;
 	var inputMed3 = document.getElementById("med3_field").value;
 	var inputSum3= document.getElementById("Sum3_field").value;
+	let user = firebase.auth().currentUser;
 
-	db.collection("Prescription").doc(inputDocnum).set({
-  patID: inputPatID,
+	db.collection("Prescription").doc(inputpatemail).set({
+  PatEmail: inputpatemail,
 	docnum: inputDocnum,
 	med1: inputMed1,
 	med2: inputMed2,
 	med3: inputMed3,
-	description: inputSum3
+	description: inputSum3,
+	DocEmail: user.uid
 
 
 
