66832c6f896fd823e0dc8f63fc2cdb77017e5221
dylanm29799 <dyevki@gmail.com>
2019-04-14 14:54:25 +0100

    Update Patient.js

--- Dr.Connect-master/public/Patient.js
+++ Dr.Connect-master/public/Patient.js
@@ -17,26 +17,47 @@ var app_firebase = {};
 var db = firebase.firestore();
 var listitem = document.querySelector("#list-item");
 var closed = document.querySelector("#closed");
+
+
 function search(){
   var patientEmail = document.getElementById("patientEmail").value;
   console.log(patientEmail);
   
   db.collection("Patient").get().then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-        if(patientEmail == doc.data().PatientEmail){
-          listitem.innerHTML = "<div><p>Patient Number: " + doc.data().PatientNumber + "<br><br> Patient Email:  " + doc.data().PatientEmail + "<br><br> Patient Address: " + doc.data().PatientAddress + "<br><br> PatientID: " + doc.data().PatID +  "</p></div>";
+        if(doc.data().PatientEmail == patientEmail){
+          listitem.innerHTML = "<div><p>Patient Name: "+ doc.data().fName + " " + doc.data().lName + "<br><br> Patient Number:"+ doc.data().PatientNumber + "<br><br> Patient Email:  " + doc.data().PatientEmail + "<br><br> Patient Address: " + doc.data().PatientAddress + "<br><br> PatientID: " + doc.data().PatID +  "</p></div>";
         console.log(doc.id, " => ", doc.data());
       
       
         }
         
+        else  if( doc.data().PatientEmail != doc.data().PatientEmail){
+          listitem.innerHTML = "<div><p> There is no information on this patient available. Please try again!</p></div>"
+              
+          
+             
+               
+            
+            
+              
+              
+              
+          
+          
+            
+          
         
+    
+        }
     });
     
       
     
 });
-  
+
+
+
   
 
 
